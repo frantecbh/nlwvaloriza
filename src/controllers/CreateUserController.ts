393f6779b384c5457b623faa30872bbd3bd45754
frantecbh <franciscomenezesbh@gmail.com>
2021-07-18 11:03:43 -0300

    Configurando Middewares de erro

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -4,6 +4,7 @@ import { CreateUserService } from '../services/CreateUserService'
 class CreateUserController {
 
   async handle(request: Request, response: Response){
+
       const { name, email, admin } = request.body
 
       const createUserService = new CreateUserService()
@@ -11,6 +12,21 @@ class CreateUserController {
       const user = await createUserService.execute({name, email, admin})
 
       return response.json(user)
+
+    // try {
+    //   const { name, email, admin } = request.body
+
+    //   const createUserService = new CreateUserService()
+
+    //   const user = await createUserService.execute({name, email, admin})
+
+    //   return response.json(user)
+      
+    // } catch (err) {
+    //   return response.status(400).json({error: err.message})
+    // }
+
+      
   }  
 
 
